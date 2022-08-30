96727e9925d2357b18053f0ffc0193170353d90e
oliver.wagner <oliver.wagner@noventi.de>
2022-08-30 17:51:46 +0200

    check application context works with SpringBootTest annotation

--- src/test/java/de/bls19/examples/springboot3check/applicationtests/Springboot3checkApplicationTests.java
+++ src/test/java/de/bls19/examples/springboot3check/applicationtests/Springboot3checkApplicationTests.java
@@ -1,13 +1,27 @@
 package de.bls19.examples.springboot3check.applicationtests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Springboot3checkApplicationTests {
+
+	@Value("${test}")
+	String myTestValue;
+
+	@Autowired
+	ApplicationContext applicationContext;
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals("aValue", myTestValue);
+		Assertions.assertNotNull(applicationContext);
 	}
 
 }
